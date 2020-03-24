a9407834e2fd979218a832780b5afa021b5edbe5
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-24 13:14:30 +1100

    added server.js file and code

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", { useNewUrlParser: true });
+
+const db = require("./models");
+
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
